3978ff7dd688e0be32a170665aecdb3097e5bb1f
gadepallavi007-del <gadepallavi007@gmail.com>
2025-10-29 11:18:53 +0530

    Add files via upload

--- LAB6/Lab6P1.java
+++ LAB6/Lab6P1.java
@@ -1,1 +1,29 @@
+/**To create an Enumeration we need to use keyword enum*/
 
+import java.util.Scanner;
+enum Fruits{
+	Apple('a'),Banana('b'),Citrus('a'),DragonFruit('d'),Fig('e'),Grapes('f');
+	char a;
+	
+	Fruits(char a)
+	{
+		this.a=a;
+	}
+	public char get(){
+		return a;
+	} 	
+}
+public class Lab6P1{
+	public static void main(String...args){
+		Scanner s = new Scanner(System.in);
+		String st = s.next();
+		char ch = st.charAt(0);
+		ch = Character.toLowerCase(ch);
+		for(Fruits f: Fruits.values())
+		{
+			if(f.get() == ch)
+			System.out.println(f);
+		}
+	}
+}				
+	
