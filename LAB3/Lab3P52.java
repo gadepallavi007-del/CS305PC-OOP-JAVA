21eba4ab1f3cabe0a5eead7766cdf97aaf8462e4
gadepallavi007-del <gadepallavi007@gmail.com>
2025-11-11 20:31:38 +0530

    Create Lab3P52.java

---
+++ LAB3/Lab3P52.java
@@ -0,0 +1,12 @@
+class Lab3P52 extends Lab3P61{
+	@Override
+	public  void demonstrate(){
+		System.out.println(" I AM A METHOD IN LAB3P62");
+	}
+	Lab3P52(){
+		super.demonstrate();
+	}
+	
+	
+	
+}
