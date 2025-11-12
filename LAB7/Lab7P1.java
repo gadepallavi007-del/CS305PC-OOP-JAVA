383d96075e5a160cb3643de565337bb2e7cce120
gadepallavi007-del <gadepallavi007@gmail.com>
2025-11-12 12:23:45 +0530

    Add files via upload

--- LAB7/Lab7P1.java
+++ LAB7/Lab7P1.java
@@ -1,1 +1,34 @@
+import java.awt.event.*;
+import java.awt.*;
+class Lab7P1{
+	public static void main(String...args){
+	final int[]a={1};
+		Frame f = new Frame();
+		FlowLayout fl = new FlowLayout();
+		f.setLayout(fl);
+		f.setSize(400,500);
+		TextField tf = new TextField(20);
+		Button b = new Button("Click");
+		b.setSize(20,50);
+		Label l = new Label();
+		l.setPreferredSize(new Dimension(20,50));
+		f.add(tf);
+		f.add(b);
+		f.add(l);
+		f.setVisible(true);
+		b.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent ae){
+			System.out.println("Hello");
+			tf.setText(tf.getText()+" "+a[0]);
+			a[0]++;
+			}
+	});
+		
+	f.addWindowListener(new WindowAdapter(){
+		public void WindowClosing(WindowEvent we){
+		f.dispose();
+			}			
+		});
 
+	}
+}		
